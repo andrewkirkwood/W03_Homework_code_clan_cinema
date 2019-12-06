8363e758f464ecb7c51c8b47f42ae54092c0c2d9
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2019-12-06 15:36:27 +0000

    show which customers are coming to see film passed

---
+++ models/customer.rb
@@ -0,0 +1,44 @@
+require_relative("../db/sql_runner")
+
+class Customer
+
+  attr_reader :id
+  attr_accessor :name, :funds
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @funds = options['funds'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM customers"
+    data = SqlRunner.run(sql)
+    return data.map{|customer| Customer.new(customer)}
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM customers;"
+    SqlRunner.run(sql)
+  end
+
+  def save()
+    sql = "INSERT INTO customers (name, funds) VALUES ($1, $2) RETURNING id"
+    values = [@name, @funds]
+    star = SqlRunner.run(sql, values).first
+    @id = star['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE customers SET (name, funds) = ($1, $2) WHERE id = $3"
+    values = [@name, @funds, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE from customers where id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+end
